b7215d7d0239bec02a44a103970875b5c41aebf9
Erald Caka <96385473+HidekiNatsumi@users.noreply.github.com>
2022-11-10 00:46:02 +0100

    Delete src directory

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Card.css";
-
-const Card = (props) => {
-  const classes = "card " + props.className; //to dinamically change the css of every sub component of the card div
-  return <div class={classes}>{props.children}</div>;
-};
-export default Card;
